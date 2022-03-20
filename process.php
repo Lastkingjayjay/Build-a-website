c9f51d9157761398a8e013290e2b0d215cad1a05
Junior R Joseph <93812419+Lastkingjayjay@users.noreply.github.com>
2022-03-20 14:32:06 -0400

    contacts required field
    added more to the php file

---
+++ process.php
@@ -0,0 +1,21 @@
+<?php ob_start();
+
+
+if(isset($_POST['submit'])) {
+
+  $to ="Juniorjosephr@gmail.com";
+  $email = $_POST ['email'] 
+  $subject = $_POST['subject']
+  $txt =$_POST['messages']
+  $headers ="From:" .$email . "\r\n" .
+  "CC: somebodyelse@example.com";
+
+
+mail($to, $subject, $headers) ;
+
+header("location: contact.html");
+
+
+}
+
+?>
