509c1e71cd77e53dbbed6acfec75ad43d303b3bd
Junior R Joseph <93812419+Lastkingjayjay@users.noreply.github.com>
2022-03-20 14:59:53 -0400

    Update process.php
    revert

--- process.php
+++ process.php
@@ -1,10 +1,23 @@
-<?php 
+<?php ob_start();
 
 
 if(isset($_POST['submit'])) {
 
   echo "it worked";
   
+  $to ="Juniorjosephr@gmail.com";
+  $email = $_POST ['email'] 
+  $subject = $_POST['subject']
+  $txt =$_POST['messages']
+  $headers ="From:" .$email . "\r\n" .
+  "CC: somebodyelse@example.com";
 
+
+mail($to, $subject, $headers) ;
+
+header("location: contact.html");
+
+
+}
 
 ?>
