4576332fb89573051c60464a241b9f9876dab47e
Junior R Joseph <93812419+Lastkingjayjay@users.noreply.github.com>
2022-03-20 14:44:28 -0400

    setting echo messge

--- process.php
+++ process.php
@@ -1,23 +1,10 @@
-<?php ob_start();
+<?php 
 
 
 if(isset($_POST['submit'])) {
 
   echo "it worked";
   
-  $to ="Juniorjosephr@gmail.com";
-  $email = $_POST ['email'] 
-  $subject = $_POST['subject']
-  $txt =$_POST['messages']
-  $headers ="From:" .$email . "\r\n" .
-  "CC: somebodyelse@example.com";
 
-
-mail($to, $subject, $headers) ;
-
-header("location: contact.html");
-
-
-}
 
 ?>
