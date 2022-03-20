d1ea0bb5ea304f1db124d8862a31a81afaa0c947
Junior R Joseph <93812419+Lastkingjayjay@users.noreply.github.com>
2022-03-20 15:24:07 -0400

    Update process.php
    second test for php submit function

--- process.php
+++ process.php
@@ -5,15 +5,14 @@ if(isset($_POST['submit'])) {
 
   echo "it worked";
   
-  $to ="Juniorjosephr@gmail.com";
-  $email = $_POST ['email'] 
-  $subject = $_POST['subject']
-  $txt =$_POST['messages']
-  $headers ="From:" .$email . "\r\n" .
+  $to = "juniorjosephr@gmai.com";
+  $subject = "My subject";
+  $txt = "Hello world!";
+  $headers = "From: webmaster@example.com" . "\r\n" .
   "CC: somebodyelse@example.com";
+  
+  mail($to,$subject,$txt,$headers);
 
-
-mail($to, $subject, $headers) ;
 
 
 
